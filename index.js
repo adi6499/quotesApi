031152cc914c42d205d6cab65947325851d8d476
adesh <mahatmeadesh291@gmail.com>
2022-08-08 15:40:24 +0530

    api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express  = require("express");
 const app  = express();
 
-const port  = process.env.port || 3000;
+const port  = process.env.PORT || 3000;
 const data = require("./data.json")
 app.get("/",(req,res)=>{
     res.send("helllo there")
